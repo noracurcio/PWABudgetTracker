722dc4002a0f7ea9dda21c9df9789da1cfd092a3
noracurcio <noracurcio@gmail.com>
2020-11-20 15:53:36 -0700

    Big ole code dump.

---
+++ Develop/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreaeIndex: true,
+  useFindAndModify: false
+};
+
+
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budger", opts)
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
